57bfd7e344e2ec30f47066633cd2cc893a427fc5
Vivienline <violine.kp@gmail.com>
2025-01-19 16:10:32 +0100

    Update: Hinzufügen von Kommentaren
    Habe einiges am Code näher dokumentiert.

--- Vorbereitung Analyse.R
+++ Vorbereitung Analyse.R
@@ -1,5 +1,4 @@
-
-setwd("C:/Users/tikva/Desktop")
+# setwd("C:/Users/tikva/Desktop")
 
 titanic <- read.csv("titanic.csv", stringsAsFactors = FALSE)
 
@@ -9,12 +8,21 @@ titanic$Title <- gsub("Mlle|Ms|Miss", "Miss", titanic$Title)
 titanic$Title <- gsub("Mme", "Mrs", titanic$Title)
 titanic$Title <- gsub("Master", "Mr", titanic$Title)
 
+
+# Kodierung von "Survived" als factor:
 titanic$Survived <- factor(titanic$Survived, levels = c(0, 1),
                            labels = c("No", "Yes"))
+
+# Kodierung von "Sex" als factor:
 titanic$Sex <- factor(titanic$Sex, levels = c("male", "female"))
+
+# Kodierung von "Embarked" als factor:
 titanic$Embarked <- factor(titanic$Embarked, levels = c("C", "Q", "S"), 
                            labels = c("Cherbourg", "Queenstown", "Southampton"))
+
+# Kodierung von "PClass" als ordered-factor:
 titanic$Pclass <- factor(titanic$Pclass, ordered = TRUE, levels = c(3, 2, 1))
+
 
 impute_age <- function(title, age) {
   if (is.na(age)) {
@@ -25,15 +33,24 @@ impute_age <- function(title, age) {
   }
 }
 titanic$Age <- mapply(impute_age, titanic$Title, titanic$Age)
+
+
+# Deck und Boardseite aus Cabin extrahieren
+# Deck: Falls leer auf NA setzen, sonst Buchstaben behalten
 titanic$Deck <- ifelse(titanic$Cabin == "", NA, substr(titanic$Cabin, 1, 1))
-titanic$Side <- ifelse(grepl("[0-9]", titanic$Cabin), 
-                       ifelse(as.numeric(gsub("[^0-9]", "", titanic$Cabin)) 
-                              %% 2 == 0, "Backbord", "Steuerbord"), 
-                       NA)
+
+# Boardseite: Anhand von Kabinennummer Side angeben, sonst auf NA setzen
+titanic$Side <- ifelse(grepl("[0-9]", titanic$Cabin),                        # Zahl enthalten?
+                       ifelse(as.numeric(gsub("[^0-9]", "", titanic$Cabin))  # entferne Buchstaben
+                       %% 2 == 0, "Backbord", "Steuerbord"), NA)             # Kodierung
+
+# Cabin: Leere "Kabinen" auf NA setzen
 titanic$Cabin <- ifelse(titanic$Cabin == "", NA, titanic$Cabin)
 
 
 titanic <- titanic[, !(names(titanic) %in% 
                          c("PassengerID", "Name", "Ticket", "Cabin"))]
+
+# Update für die csv-Datei
 write.csv(titanic, "processed_titanic.csv", row.names = FALSE)
 
