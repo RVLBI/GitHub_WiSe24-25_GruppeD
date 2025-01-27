63a32995c1dc8bee3a352db6122c5db72bf35a86
Lollerskates5 <mikadirkes@gmail.com>
2025-01-27 15:02:08 +0100

    Erster Entwurf für Funktionen-R-Skript 1.R

---
+++ Funktionen-R-Skript 1.R
@@ -0,0 +1,33 @@
+# Aufgabe 2a)
+
+# i:
+
+
+
+# ii:
+
+
+
+# iii:
+
+
+
+# iv: 
+# bivariate_stats_md  - Funktion zur Berechnung und Ausgabe von geeigneten  
+#                      deskriptiven bivariaten Statistiken fuer den Zusammenhang  
+#                      zwischen einer metrischen und einer dichotomen Variable
+#
+# Input: metric_var   - Beobachtungsvektor der metrischen Variable
+#        dichotom_var - Beobachtungsvektor der dichotomen Variable
+# 
+# Output: ...
+
+bivariate_stats_md <- function(metric_var, dichotom_var) {
+  # ...
+}
+
+
+
+# v:
+
+
