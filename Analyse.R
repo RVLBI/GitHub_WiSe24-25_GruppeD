b263880db002869494f7b6ae97a4d6baceede1d5
Lollerskates5 <mikadirkes@gmail.com>
2025-02-06 14:28:21 +0100

    Erster Entwurf Analyse

---
+++ Analyse.R
@@ -0,0 +1,18 @@
+# Zu untersuchende Statistiken und deren Typen: 
+
+# Survived - dichotom
+# Pclass   - kategorial
+# Sex      - dichotom
+# Age      - metrisch
+# Sibsp    - metrisch
+# Parch    - metrisch
+# Fare     - metrisch
+# Embarked - kategorial
+# Title    - kategorial
+# Deck     - kategorial
+# Side     - dichotom
+
+
+# [Funktionen aus Funktionen-R-Skript 1.R auf jeweils passende Statistiken anwenden]
+
+# Man sieht, dass... [ausgegebene Werte der angewandten Funktionen deuten]
