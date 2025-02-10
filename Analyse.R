e4761747a55c4806fb2eae906a1e99d796c37340
Lollerskates5 <mikadirkes@gmail.com>
2025-02-10 13:38:27 +0100

    Erstes Beispiel für Analysen-Teil

--- Analyse.R
+++ Analyse.R
@@ -1,3 +1,10 @@
+# setwd("/Users/mikadirkes/Github-Projekt/")
+
+titanic <- read.csv("processed_titanic.csv")
+
+source("Funktionen-R-Skript 1.R")
+source("Funktionen-R-Skript 2.R")
+
 # Zu untersuchende Statistiken und deren Typen: 
 
 # Survived - dichotom
@@ -13,6 +20,13 @@
 # Side     - dichotom
 
 
-# [Funktionen aus Funktionen-R-Skript 1.R auf jeweils passende Statistiken anwenden]
+bivariate_stats_md(titanic$Fare, titanic$Survived)
+# Man sieht, dass es einen signifikanten Unterschied im erwarteten Ticketpreis
+# zwischen denen, die überlebt haben, und denen, die nicht überlebt haben, gibt.
+# Der Ticketpreis von denen, die überlebt haben, war durchschnittlich mehr als
+# doppelt so teuer wie von den Verstorbenen. Es besteht allerdings auch eine 
+# deutlich größere Varianz in den Ticketpreisen der Überlebenden, es gab also,
+# wie man gut dem Boxplot entnehmen kann, auch viele Überlebende mit günstigen
+# Boardkarten. 
 
-# Man sieht, dass... [ausgegebene Werte der angewandten Funktionen deuten]
+#...
