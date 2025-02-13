52b8aab7f55b0c0f5ae4deb9d7eebc957ad299d3
Vivienline <violine.kp@gmail.com>
2025-02-13 14:56:55 +0100

    Funktionen für Nr.2, i
    Funktionen fast fertig, muss noch testen und die Ergebnisse ausgeben lassen (für Modus habe ich Ronas Funktion benutzt, deswegen Rona als Co-Autor)
    
    Co-Authored-By: Rona <191261692+RVLBI@users.noreply.github.com>

--- Funktionen-R-Skript 2.R
+++ Funktionen-R-Skript 2.R
@@ -21,3 +21,24 @@ Balkendiagramm_kathegorial <- function(x) {
           xlab = "Kategorie",
           ylab = "Häufigkeit")
 }
+
+
+# Berechnung der Schiefe:
+
+schiefefkt <- function(arith_m, med, sd) {
+  # benutze Formel sk3 aus deskriptive Statistik: (xquer - med)/sd
+  ergebnis <- (arith_m - med)/sd
+  return(ergebnis)
+}
+
+
+# Berechnung der Wölbung:
+
+woelbungfkt <- function(variable, qd) {
+  # benutze Formel aus deskr. Stat. für explorativen Wölbungskoeffizient
+  Q9 <- quantile(variable, probs = 0.9, names = F, na.rm = T)
+  Q1 <- quantile(variable, probs = 0.1, names = F, na.rm = T)
+  ergebnis <- 0.5 * (qd/(Q9-Q1))
+  return(ergebnis)
+}
+  
